dd4fd0514c5709042e0f0fcbbaff3635fb745ee7
jrdnr <jrdn@ritzmob.com>
2021-04-30 06:51:26 -0400

    Create Import-SyncroModule.ps1
    Simple function to import Syncro's Module outside of Syncro

---
+++ Import-SyncroModule.ps1
@@ -0,0 +1,17 @@
+function Import-SyncroModule {
+    param (
+        #Defaults to the UUID of local system but you can provide the UUID of your test system.
+        $UUID = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\RepairTech\Syncro' -Name uuid -ErrorAction Stop).uuid
+    )
+
+    # Set up $env: vars for Syncro Module
+    $env:SyncroModule               = "$env:ProgramData\Syncro\bin\module.psm1"
+    $env:RepairTechApiBaseURL       = 'syncromsp.com'
+    $env:RepairTechApiSubDomain     = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\RepairTech\Syncro' -Name shop_subdomain).shop_subdomain
+    $env:RepairTechFilePusherPath   = 'C:\ProgramData\Syncro\bin\FilePusher.exe'
+    $env:RepairTechUUID             = $UUID
+
+    Import-Module -Name $env:SyncroModule -WarningAction SilentlyContinue
+
+    $env:RepairTechApiBaseURL = $env:RepairTechApiSubDomain = $env:RepairTechFilePusherPath = $env:RepairTechUUID = $null
+}
