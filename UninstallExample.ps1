85db3b475c691a3a36a37fd3ce7481001338161c
jrdnr <jrdn@ritzmob.com>
2022-02-02 16:35:13 -0500

    Improve Nable cleanup and uninstall

--- UninstallExample.ps1
+++ UninstallExample.ps1
@@ -1,22 +1,44 @@
 #Example application uninstall. Not all apps register in either of these locations but this will work for some apps
 
-$AppSearch = 'screenconnect*'
-
+$AppSearch = 'Screenconnect'
+$Publisher = 'MspPlatform|N-able|SolarWinds|Solve IT'
+$SilentUninstallFlag = '/SILENT'
+$exit = 0
 function Get-InstalledApps {
     param (
-        [string]$App = '*',
-        [string]$Publisher = '*'
+        [string]$App,
+        [string]$Publisher
     )
     $installLocation = @(
         "HKLM:\software\microsoft\windows\currentversion\uninstall"
         "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\"
     )
-    foreach ($il in $installLocation){
-        get-childitem $il | ForEach-Object { Get-ItemProperty $_.PSPath } |
-            #Select-Object DisplayVersion,InstallDate,ModifyPath,Publisher,UninstallString,Language,DisplayName |
-            Where-Object {$_.DisplayName -like $App -and $_.Publisher -like $Publisher}
+    $AllApps = get-childitem $installLocation | ForEach-Object { Get-ItemProperty $_.PSPath }
+        #| Select-Object DisplayVersion,InstallDate,ModifyPath,Publisher,UninstallString,Language,DisplayName
+
+    foreach ($a in $AllApps){
+        if ($a.DisplayName -match $App -and $a.Publisher -match $Publisher){
+            $a
+        }
     }
 }
 
-$cmd = (Get-InstalledApps $AppSearch).UninstallString -split ' '
-Start-Process $cmd[0] -ArgumentList "$($cmd[1]) /qn"
+$Applist = Get-InstalledApps -Publisher $Publisher
+foreach ($a in $Applist){
+    'Uninstalling "{0}" by "{1}"' -f $a.DisplayName, $a.Publisher
+    if($a.QuietUninstallString -match '"(.*)"\s(/.*)'){
+        Start-Process -FilePath $Matches[1] -ArgumentList $Matches[2] -Wait
+    } elseif ($a.UninstallString -like 'C:\Program Files*'){
+        Start-Process -FilePath $a.UninstallString -ArgumentList $SilentUninstallFlag -Wait
+    } elseif ($a.UninstallString -like 'MsiExec.exe*'){
+        $ArgList = (($a.UninstallString -split 'MsiExec.exe')[1].trim() -replace '/I{','/X{').ToString()
+        $ArgList = '{0} /quiet /norestart' -f $ArgList
+        Start-Process -FilePath MsiExec.exe -ArgumentList $ArgList -Wait
+    } else {
+        "Could not auto uninstall $($a.DisplayName)"
+        "Uninstall String: '$($a.UninstallString)'"
+        $exit = 2
+    }
+}
+
+exit $exit
--- nable_Cleanup.ps1
+++ nable_Cleanup.ps1
@@ -1,7 +1,22 @@
+$Publisher = 'MspPlatform|N-able|SolarWinds|Solve IT'
+$SilentUninstallFlag = '/SILENT'
+$exit = 0
+
 $FileLocation = @(
     'C:\Program Files\'
     'C:\Program Files (x86)\'
     'C:\ProgramData'
+)
+
+$folderList = @(
+    'Avast*'
+    'BeAnywhere'
+    'Level Platforms'
+    'MspPlatform'
+    'N-able*'
+    'Package Cache'
+    'Rmm'
+    'SolarWinds*'
 )
 
 function Import-SyncroModule {
@@ -19,29 +34,43 @@ function Import-SyncroModule {
 
     Import-Module -Name $env:SyncroModule -WarningAction SilentlyContinue
 }
+
 function Get-InstalledApps {
     param (
-        [string]$App = '*',
-        [string]$Publisher = '*'
+        [string]$App,
+        [string]$Publisher
     )
     $installLocation = @(
         "HKLM:\software\microsoft\windows\currentversion\uninstall"
         "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\"
     )
-    foreach ($il in $installLocation){
-        get-childitem $il | ForEach-Object { Get-ItemProperty $_.PSPath } |
-            #Select-Object DisplayVersion,InstallDate,ModifyPath,Publisher,UninstallString,Language,DisplayName |
-            Where-Object {$_.DisplayName -like $App -and $_.Publisher -like $Publisher}
+    $AllApps = get-childitem $installLocation | ForEach-Object { Get-ItemProperty $_.PSPath }
+
+    foreach ($a in $AllApps){
+        if ($a.DisplayName -match $App -and $a.Publisher -match $Publisher){
+            $a
+        }
     }
 }
 
-$nable = Get-InstalledApps -App n-able
-
-if($null -eq $nable){
-    foreach ($l in $FileLocation) {
-        Get-ChildItem -Path $l -Filter N-Able* | Remove-Item -Recurse -Force
+$Applist = Get-InstalledApps -Publisher $Publisher
+foreach ($a in $Applist){
+    'Uninstalling "{0}" by "{1}"' -f $a.DisplayName, $a.Publisher
+    if($a.QuietUninstallString -match '"(.*)"\s(/.*)'){
+        Start-Process -FilePath $Matches[1] -ArgumentList $Matches[2] -Wait
+    } elseif ($a.UninstallString -like 'C:\Program Files*'){
+        Start-Process -FilePath $a.UninstallString -ArgumentList $SilentUninstallFlag -Wait
+    } elseif ($a.UninstallString -like 'MsiExec.exe*'){
+        $ArgList = (($a.UninstallString -split 'MsiExec.exe')[1].trim() -replace '/I{','/X{').ToString()
+        $ArgList = '{0} /quiet /norestart' -f $ArgList
+        Start-Process -FilePath MsiExec.exe -ArgumentList $ArgList -Wait
+    } else {
+        "Could not auto uninstall $($a.DisplayName)"
+        "Uninstall String: '$($a.UninstallString)'"
+        $exit = 2
     }
-} else {
-    Import-SyncroModule
-    Rmm-Alert -Category 'N-Able Installed' -Body "Uninstall N-Able`n$($nable | out-string)"
 }
+
+Get-ChildItem -Path $FileLocation -Include $folderList | Remove-Item -Recurse -Force -ErrorAction Continue
+
+exit $exit
