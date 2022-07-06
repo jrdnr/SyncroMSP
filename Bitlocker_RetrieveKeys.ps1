de918916ed468fd90a749d454a3712253da81ed9
jrdnr <jrdn@ritzmob.com>
2022-07-06 09:48:42 -0400

    Clean up loops and seperate out try block

--- Bitlocker_RetrieveKeys.ps1
+++ Bitlocker_RetrieveKeys.ps1
@@ -11,23 +11,6 @@ try{
     # Get Bitlocker Volumes or exit.
     $BitLockerVolume = Get-BitLockerVolume -ErrorAction stop
 
-    $Report = $BitLockerVolume |
-        ForEach-Object {
-            $MountPoint = $_.MountPoint
-            $_.KeyProtector | Where-Object {$_.RecoveryPassword -notmatch '^\s$' -and $_.RecoveryPassword.Length -gt 5} |
-                ForEach-Object {
-                    Write-Output "$MountPoint Id:$($_.KeyProtectorId), key:$($_.RecoveryPassword)"
-                }
-        } | Out-String
-
-    if ($Report.Length -ge 56){
-        if ($env:SyncroModule){
-            Import-Module $env:SyncroModule
-            Set-Asset-Field -Name "Bitlocker Drives" -Value $Report
-        } else {
-            $Report
-        }
-    }
 } catch [System.Management.ManagementException] {
     "Bitlocker Not Detected"
 } catch [System.Management.Automation.CommandNotFoundException] {
@@ -36,4 +19,22 @@ try{
     "Unexpected Error"
     (($Error[0] | Select-Object -Property Exception | Out-String).trim() -split '\r')[-1]
     exit 1
+}
+
+$Report = $BitLockerVolume |
+ForEach-Object {
+    $MountPoint = $_.MountPoint
+    $_.KeyProtector | Where-Object {$_.RecoveryPassword -notmatch '^\s$' -and $_.RecoveryPassword.Length -gt 5} |
+        ForEach-Object {
+            Write-Output "$MountPoint Id:$($_.KeyProtectorId), key:$($_.RecoveryPassword)"
+        }
+} | Out-String
+
+if ($Report.Length -ge 56){
+    if ($env:SyncroModule){
+        Import-Module $env:SyncroModule
+        Set-Asset-Field -Name "Bitlocker Drives" -Value $Report
+    } else {
+        $Report
+    }
 }
