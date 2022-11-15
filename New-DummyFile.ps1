a8de1bc53819b52c6cf666b66c8309a87c0901da
jrdnr <jrdn@ritzmob.com>
2022-11-15 11:27:31 -0500

    fix duplicate file issue and out of space

--- New-DummyFile.ps1
+++ New-DummyFile.ps1
@@ -2,28 +2,38 @@ function New-DummyFile {
     param(
         [int]$FileCount = 1,
         [int64]$FileBytes,
-        [string]$Folder = 'C:\',
+        [string]$Folder = $PWD,
         $name = 'DummyFile',
         $extension = 'txt'
     )
 
     # Check for input
     if (Test-Path $folder){
+        #Set-Location -Path $Folder -ErrorAction Continue
         if ($extension -contains '.') {
             $extension = $extension.Substring(($extension.LastIndexOf(".") + 1), ($extension.Length - 1))
         }
 
         foreach($i in (1..$FileCount)) {
-            $path = $folder + '\' + $name + '_' + $i + '.' + $extension
-            $f = new-object System.IO.FileStream $path, Create, ReadWrite
-            $f.SetLength($FileBytes)
-            $f.Close()
+            do {
+                $path = $folder + '\' + $name + $r + '_' + $i + '.' + $extension
+                $r++
+            } while (Test-Path -Path $path)
+
+            try {
+                $f = new-object System.IO.FileStream $path, Create, ReadWrite
+                $f.SetLength($FileBytes)
+                $f.Close()
+            }
+            catch {
+                Get-PSDrive -Name ($Folder.split(':')[0])
+                return "$i files created"
+            }
 
             Start-Sleep -Seconds 0.5
         }
 
     } else {
         Write-Warning "The folder $folder doesn't exist"
-        Exit
     }
 }
