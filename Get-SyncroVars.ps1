6132698ad4dcf978c872b5b9c62bf5357e358496
jrdnr <jrdn@ritzmob.com>
2023-09-28 15:53:02 -0400

    add simplified version

--- Get-SyncroVars.ps1
+++ Get-SyncroVars.ps1
@@ -44,9 +44,10 @@ function Get-SyncroVars {
     }
 }
 
-<# Non Function Version
+<# Non Function Versions
+#region Dynamic logger
 $SyncroVars = foreach ($line in (Get-Content -Path $MyInvocation.MyCommand.Path -ErrorAction SilentlyContinue)){
-    if ($line -like '#- Start of Script -#') {
+    if ($line -like '*#- Start of Script -#*') {
         break
     } else {
         # Mask Guid in log
@@ -72,4 +73,19 @@ if ($SyncroVars.Count -ge 1){
     '{0}{1}{0}' -f $FootPad,$FootTxt
     ''
 }
+#endregion Dynamic logger
+
+#region Simple logger
+'============ Syncro Inserted Code ============'
+foreach ($line in (Get-Content -Path  $MyInvocation.MyCommand.Path -ErrorAction Stop)){
+    if ($line -like '*#- Start of Script -#*') {
+        break
+    } else {
+        # Mask Guid in log
+        $line -replace '([0-9a-fA-F]{3})[0-9a-fA-F]{5}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{8}([0-9a-fA-F]{4})|^(\w{3})\w{23,}(\w{4})$', '$1$3*****-****-****-****-********$2$4'
+    }
+}
+'============== END Syncro Code ==============='
+''
+#endregion Simple logger
 #>
