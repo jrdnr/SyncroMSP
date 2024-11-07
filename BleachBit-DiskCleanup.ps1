bdfbe29ec82afee43b18c5394b1bfbd4527a7442
AndyAIT <153778124+AndyAIT@users.noreply.github.com>
2024-11-07 13:26:43 +1100

    adjust for Gorelo

--- BleachBit-DiskCleanup.ps1
+++ BleachBit-DiskCleanup.ps1
@@ -1,17 +1,3 @@
-# Set up $env: vars for Syncro Module
-if($env:SyncroModule -match '^\s*$'){
-    $SyncroRegKey = Get-ItemProperty -Path 'HKLM:\SOFTWARE\WOW6432Node\RepairTech\Syncro' -Name uuid, shop_subdomain
-    $env:RepairTechFilePusherPath   = 'C:\ProgramData\Syncro\bin\FilePusher.exe'
-    $env:RepairTechKabutoApiUrl     = 'https://rmm.syncromsp.com'
-    $env:RepairTechSyncroApiUrl     = 'https://{subdomain}.syncromsp.com'
-    $env:RepairTechSyncroSubDomain  = $SyncroRegKey.shop_subdomain
-    $env:RepairTechUUID             = $SyncroRegKey.uuid
-    $env:SyncroModule               = "$env:ProgramData\Syncro\bin\module.psm1"
-}
-if (Test-Path -Path $env:SyncroModule) {
-    Import-Module -Name $env:SyncroModule -WarningAction SilentlyContinue
-}
-
 $workingdir = $env:TEMP
 If(!(test-path $workingdir))
 {
@@ -25,7 +11,7 @@ $Before = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" 
 @{ Name = "PercentFree" ; Expression = {"{0:P1}" -f( $_.FreeSpace / $_.Size ) } } |
 Format-Table -AutoSize | Out-String
 # Perform Cleanup Tasks
-Expand-Archive $workingdir\bleachbit-portable.zip -DestinationPath $workingdir -Force
+Expand-Archive $$gorelo:file.bleachbit-portable.zip -DestinationPath $workingdir -Force
 $Params = "-c adobe_reader.cache adobe_reader.tmp chromium.cache chromium.history chromium.vacuum firefox.cache firefox.url_history firefox.vacuum flash.cache flash.cookies google_chrome.cache google_chrome.history google_chrome.vacuum internet_explorer.cache internet_explorer.downloads java.cache opera.cache opera.history opera.vacuum silverlight.temp silverlight.cookies system.recycle_bin system.tmp system.memory_dump system.logs windows_explorer.mru winrar.history winrar.temp"
 $ParsedParams = $Params.Split(" ")
 & $workingdir\Bleachbit-Portable\bleachbit_console.exe $ParsedParams
@@ -38,10 +24,10 @@ $After = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq "3" }
 Format-Table -AutoSize | Out-String
 $LogDate = get-date -format "MM-d-yy-HH"
 #Start-Transcript -Path $workingdir\$LogDate.log
-#write-output "Space Before"
-#write-output $Before
-#write-output "space After"
-#write-output $After
+write-output "Space Before"
+write-output $Before
+write-output "space After"
+write-output $After
 #stop-transcript
 $Disk += "$($LogDate)`rBefore$($Before)After$($After)"
-Set-Asset-Field -Name "Weekly Temp Files" -Value $Disk | Out-Null
+
